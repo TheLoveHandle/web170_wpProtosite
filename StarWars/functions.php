63b78e892620615c68136dbc845bf95256579aa7
TheLoveHandle <adanie04@seattlecentral.edu>
2016-05-25 17:21:27 -0700

    Week 8 upload
    added flex slider stuff and finished week 8 requirements

--- StarWars/functions.php
+++ StarWars/functions.php
@@ -1,9 +1,13 @@
 <?php
 
-// Register My Menus
+// Register the nav menu
 register_nav_menus(array(
-'main-menu' => __( 'Main' ),
+'main-menu' => __('Main'),
 ));
-//
 
+// Support for themes
+add_theme_support('post-thumbnails');
+
+// Support for excerpts
+add_post_type_support('page', 'excerpt');
 ?>
