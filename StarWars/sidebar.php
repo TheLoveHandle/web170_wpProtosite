c57cc97f5936e7931f2ae1b7a1e5f6fe1192fd40
TheLoveHandle <adanie04@seattlecentral.edu>
2016-06-01 18:24:43 -0700

    Add files via upload

--- StarWars/sidebar.php
+++ StarWars/sidebar.php
@@ -1,3 +1,8 @@
 <aside>
+    
+    <?php if(get_post_meta($post->ID, 'Quote', true));?>
+    
+    <blockquote><p><?php echo get_post_meta($post->ID, 'Quote', true);?></p></blockquote>
+    
     <img id="asidePhoto" src="<?php bloginfo('template_directory') ?>/images/SidebarPhoto.jpg" alt="aside">
 </aside>
